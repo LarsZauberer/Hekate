59b9ef7c8417b02de4f5d9abc0d42a049f074db4
LarsZauberer <wasser.ian@gmail.com>
2021-07-12 22:55:23 +0200

    Dynamic Tests

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class App(Application):
     def update(self, task):
         super().update(task)
         radioLight.pos = (dynamic.node.getPos().x, dynamic.node.getPos().y, dynamic.node.getPos().z + 2)
-        """if dynamic.node.getPos().z < 2:
-            dynamic.node.node().setLinearVelocity(Vec3(0, 0, 10))"""
+        if dynamic.node.getPos().z < 2:
+            dynamic.node.node().setLinearVelocity(Vec3(0, 0, 10))
         return task.cont
         
 app = App("Test", True)
@@ -28,7 +28,7 @@ go2.node.setR(90)
 dynamic = GameObject(app, "dynamic", "radioactive.bam", mass=10)
 dynamic.node.setPos(0, 0, 5)
 
-dynamic.node.node().setLinearVelocity(Vec3(15, 0, 0))
+# dynamic.node.node().setLinearVelocity(Vec3(15, 0, 0))
 
 my_light = PointLight()
 my_light.energy = 5000
