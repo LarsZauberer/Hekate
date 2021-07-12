c0aa936a2ae44dd15bd980904de7d03c2061cba1
LarsZauberer <wasser.ian@gmail.com>
2021-07-12 22:55:32 +0200

    Model Fix

--- src/GameObjects/GameObject.py
+++ src/GameObjects/GameObject.py
@@ -8,7 +8,6 @@ class GameObject:
     def __init__(self, app, name="undefined", model=None, x=0, y=0, z=0, rx=0, ry=0, rz=0, sx=0, sy=0, sz=0, collisionShape=BulletBoxShape(Vec3(0.5, 0.5, 0.5)), mass=0):
         self.app = app
         self.collisionShape = collisionShape
-        self.model = Path("Content") / Path(model)
         
         node = BulletRigidBodyNode(name)
         node.setMass(mass)
@@ -16,13 +15,18 @@ class GameObject:
         self.node = self.app.render.attachNewNode(node)
         self.app.world.attachRigidBody(node)
         
+        self.app.objectRegistry.append(self)
+        
         # TODO: Load Model multithreaded
-        if self.model != None:
+        if model != None:
+            self.model = Path("Content") / Path(model)
             modelObj = self.app.loader.loadModel(self.model)
             modelObj.copyTo(self.node)
             # I'm not really sure what that line is doing. :D
             self.app.render_pipeline.prepare_scene(modelObj)
+        else:
+            self.model = None
     
-    def update():
-        raise NotImplementedError
+    def update(self):
+        pass
         
