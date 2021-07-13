8c3e3620147229148770b7399195c9c2e1dc9402
LarsZauberer <wasser.ian@gmail.com>
2021-07-13 22:17:19 +0200

    NoClip

--- src/GameObjects/Player/FirstPersonPlayer.py
+++ src/GameObjects/Player/FirstPersonPlayer.py
@@ -14,6 +14,8 @@ class FirstPersonPlayer(DynamicObject):
         w = 0.4
         shape = BulletCapsuleShape(w, h - 2 * w, ZUp)
         shape2 = BulletBoxShape((1, 1, 1))
+        
+        self.noClip = False
 
         super().__init__(app, name="Player", collisionShape=shape, x=-10, y=-10, z=14, mass=10, model="Block.bam")
     
@@ -36,70 +38,69 @@ class FirstPersonPlayer(DynamicObject):
             if self.app.camera.getP() < -90:
                 self.app.camera.setP(-90)
         
-        speed = Vec3(0, 0, 0)
+        if self.noClip == False:
+            # Normal Movement
+            speed = Vec3(0, 0, 0)
+            
+            if "w" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(0,1,0))
+                forward.normalize()
+                forward.z = 0
+                speed += forward*self.speed
+            if "s" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(0,-1,0))
+                forward.normalize()
+                forward.z = 0
+                speed += forward*self.speed
+            if "d" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(1,0,0))
+                forward.normalize()
+                forward.z = 0
+                speed += forward*self.speed
+            if "a" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(-1,0,0))
+                forward.normalize()
+                forward.z = 0
+                speed += forward*self.speed
+            if "space" in self.app.keys and self.isGrounded():
+                speed.z += 20
+            
+            if self.isGrounded():
+                self.node.node().setLinearVelocity((speed.x, speed.y, self.node.node().getLinearVelocity().z + speed.z))
+            else:
+                self.node.node().setLinearVelocity((self.node.node().getLinearVelocity().x + speed.x/self.speed, self.node.node().getLinearVelocity().y + speed.y/self.speed, self.node.node().getLinearVelocity().z + speed.z))
+                if self.node.node().getLinearVelocity().x > self.speed:
+                    self.node.node().setLinearVelocity((self.speed, self.node.node().getLinearVelocity().y, self.node.node().getLinearVelocity().z))
+                if self.node.node().getLinearVelocity().x < -self.speed:
+                    self.node.node().setLinearVelocity((-self.speed, self.node.node().getLinearVelocity().y, self.node.node().getLinearVelocity().z))
+                if self.node.node().getLinearVelocity().y > self.speed:
+                    self.node.node().setLinearVelocity((self.node.node().getLinearVelocity().x, self.speed, self.node.node().getLinearVelocity().z))
+                if self.node.node().getLinearVelocity().y < -self.speed:
+                    self.node.node().setLinearVelocity((self.node.node().getLinearVelocity().x, -self.speed, self.node.node().getLinearVelocity().z))
         
-        if "w" in self.app.keys:
-            forward = self.app.render.getRelativeVector(self.app.camera, Vec3(0,1,0))
-            forward.normalize()
-            forward.z = 0
-            speed += forward*self.speed
-        if "s" in self.app.keys:
-            forward = self.app.render.getRelativeVector(self.app.camera, Vec3(0,-1,0))
-            forward.normalize()
-            forward.z = 0
-            speed += forward*self.speed
-        if "d" in self.app.keys:
-            forward = self.app.render.getRelativeVector(self.app.camera, Vec3(1,0,0))
-            forward.normalize()
-            forward.z = 0
-            speed += forward*self.speed
-        if "a" in self.app.keys:
-            forward = self.app.render.getRelativeVector(self.app.camera, Vec3(-1,0,0))
-            forward.normalize()
-            forward.z = 0
-            speed += forward*self.speed
-        if "space" in self.app.keys and self.isGrounded():
-            speed.z += 20
-        
-        print(speed, self.node.getPos())
-        
-        if self.isGrounded():
-            self.node.node().setLinearVelocity((speed.x, speed.y, self.node.node().getLinearVelocity().z + speed.z))
         else:
-            self.node.node().setLinearVelocity((self.node.node().getLinearVelocity().x + speed.x/self.speed, self.node.node().getLinearVelocity().y + speed.y/self.speed, self.node.node().getLinearVelocity().z + speed.z))
-            if self.node.node().getLinearVelocity().x > self.speed:
-                self.node.node().setLinearVelocity((self.speed, self.node.node().getLinearVelocity().y, self.node.node().getLinearVelocity().z))
-            if self.node.node().getLinearVelocity().x < -self.speed:
-                self.node.node().setLinearVelocity((-self.speed, self.node.node().getLinearVelocity().y, self.node.node().getLinearVelocity().z))
-            if self.node.node().getLinearVelocity().y > self.speed:
-                self.node.node().setLinearVelocity((self.node.node().getLinearVelocity().x, self.speed, self.node.node().getLinearVelocity().z))
-            if self.node.node().getLinearVelocity().y < -self.speed:
-                self.node.node().setLinearVelocity((self.node.node().getLinearVelocity().x, -self.speed, self.node.node().getLinearVelocity().z))
-        """print(dir(self.character))
-        newSpeed = self.preJumpSpeed
-        if self.preJumpSpeed.x == 0:
-            newSpeed.x = speed.x*0.5
-        if self.preJumpSpeed.y == 0:
-            newSpeed.y = speed.y*0.5
-        print(newSpeed)
-        self.character.setLinearMovement(newSpeed, True)"""
-        """maxAirVel = self.speed*0.3
-        newSpeed = self.preJumpSpeed*0.3
-        if speed.x != 0:
-            newSpeed.x = speed.x*0.7
-        if speed.y != 0:
-            newSpeed.y = speed.y*0.7
-        if newSpeed.x > self.preJumpSpeed.x*0.3:
-            newSpeed.x = self.preJumpSpeed.x*0.3
-        if newSpeed.y > self.preJumpSpeed.y*0.3:
-            newSpeed.y = self.preJumpSpeed.y*0.3"""
-        """newSpeed = self.preJumpSpeed + speed
-        if newSpeed > 0.5:
-            newSpeed.normalize()
-            newSpeed *= 0.5
-        self.character.setLinearMovement(newSpeed, True)"""
-        
-        print(self.isGrounded())
+            # Noclip
+            speed = Vec3(0, 0, 0)
+            if "w" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(0,1,0))
+                forward.normalize()
+                speed += forward
+            if "s" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(0,-1,0))
+                forward.normalize()
+                speed += forward
+            if "d" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(1,0,0))
+                forward.normalize()
+                speed += forward
+            if "a" in self.app.keys:
+                forward = self.app.render.getRelativeVector(self.app.camera, Vec3(-1,0,0))
+                forward.normalize()
+                speed += forward
+            
+            self.node.setPos(self.node.getPos() + speed)
+            self.node.node().setLinearVelocity((0, 0, 0))
+            
         
         self.app.win.movePointer(0, int(self.app.win.getXSize()/2), int(self.app.win.getYSize()/2))
         
