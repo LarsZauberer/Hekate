7dc4b422f0d9d2e234be83d9f799d602d04988d9
LarsZauberer <wasser.ian@gmail.com>
2021-07-19 12:23:23 +0200

    Setup File

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="Hekate Engine Game",
+    options = {
+        'build_apps': {
+            'include_patterns': [
+                'Content/*/*.bam',
+                'Content/*/*.json',
+                'Content/*/*.png',
+                'Content/*.bam',
+                'Content/*.json',
+                'Content/*.png',
+            ],
+            # TODO: Change to gui_application if the logging is finished
+            'console_apps': {
+                'Hekate Engine Game': 'main.py',
+            },
+            'plugins': [
+                "pandagl",
+                "p3openal_audio",
+            ],
+            'platforms': [
+                'win_amd64',
+            ]
+        }
+    },
+)
