d8bec3420b8c56632d2d064985374398d5e74433
LarsZauberer <wasser.ian@gmail.com>
2021-07-23 12:35:54 +0200

    Log setup and log statements in application

--- src/Application.py
+++ src/Application.py
@@ -4,6 +4,8 @@ from panda3d.core import load_prc_file_data, Vec3
 from panda3d.bullet import BulletWorld, BulletDebugNode
 from direct.gui.DirectGui import DirectEntry
 from pathlib import Path
+import logging
+from rich.logging import RichHandler
 
 # Plugins for packaging
 from rpplugins import ao, bloom, clouds, color_correction, dof, env_probes, forward_shading, fxaa, motion_blur, pssm, scattering, skin_shading, sky_ao, smaa, ssr, volumetrics, vxgi
@@ -33,19 +35,39 @@ class Application(ShowBase):
     def __init__(self, prcName, debug=False):
         # The main game class. Storing everything from world to models
         
+        FORMAT = "[%(name)s] %(message)s"
+        if debug:
+            logging.basicConfig(
+                level="DEBUG", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+            )
+        else:
+            logging.basicConfig(
+                level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+            )
+
+        self.log = self.getLogger("root")
+        self.log.debug(f"Logging Initialized")
         
         load_prc_file_data("", f"""
             win-size 1920 1080
             window-title {prcName}
         """)
 
-        # Construct and create the pipeline
-        self.render_pipeline = RenderPipeline()
-        self.render_pipeline.set_loading_screen_image("Content/loadingScreen.png")
-        self.render_pipeline.create(self)
-        
-        self.createBulletWorld()
-        self.doPhysics = True
+        try:
+            # Construct and create the pipeline
+            self.render_pipeline = RenderPipeline()
+            self.render_pipeline.set_loading_screen_image("Content/loadingScreen.png")
+            self.render_pipeline.create(self)
+            self.log.debug(f"Render Pipeline started")
+        except Exception:
+            self.log.exception(f"Error while creating render_pipeline")
+            
+        try:
+            self.createBulletWorld()
+            self.log.debug(f"Created Physics World")
+            self.doPhysics = True
+        except Exception:
+            self.log.exception(f"Error while creating physics world")
         
         self.taskMgr.add(self.update, "update")
         
@@ -58,7 +80,10 @@ class Application(ShowBase):
         self.mv = self.mouseWatcherNode
         
         self.noclip = False
-            
+        
+        self.log.debug(f"Assigned Variables")
+        
+        # TODO: #26 Keybinding System
         self.accept("w", self.keys.append, ["w"])
         self.accept("w-up", self.keys.remove, ["w"])
         self.accept("s", self.keys.append, ["s"])
@@ -74,12 +99,19 @@ class Application(ShowBase):
 
         self.accept("tab", self.show_Console)
         
+        self.log.debug(f"Assigned Keybinds")
+        
         
         # Finished -> Loading Map
         # TODO: #23 Make map list variable
         maps = {"test": Path("Content/map.json"), "test2": Path("map.json")}
         self.mapLoader = MapLoader(self, maps)
-        self.mapLoader.loadMap("test")
+        self.log.debug(f"Created Maploader")
+        try:
+            self.mapLoader.loadMap("test")
+        except Exception:
+            self.log.exception(f"Error while loading map: test")
+        self.log.info(f"Loading Map: test")
         
     
     def update(self, task):
@@ -93,12 +125,15 @@ class Application(ShowBase):
     def show_Console(self):
         self.entry = DirectEntry(text = "", scale=.05, command=self.execute,
         initialText="", numLines = 1, focus=1, pos=Vec3(0.8, 0, -0.95))
+        self.log.debug(f"Showing Console")
     
     def execute(self, cmd):
         cmd = cmd.lower()
+        self.log.info(f"Trying to execute command: {cmd}")
         self.entry.destroy()
         if "noclip" in cmd:
             if not self.noclip:
+                self.log.info(f"Activating noclip")
                 self.noclip = True
                 from rpcore.util.movement_controller import MovementController
                 self.controller = MovementController(self)
@@ -108,10 +143,15 @@ class Application(ShowBase):
                 self.controller.setup()
                 self.taskMgr.remove("Player_update")
             else:
+                self.log.info(f"Deactivating noclip")
                 self.noclip = False
                 self.controller = False
                 self.taskMgr.add(self.player.update, "Player_update")
         elif "show triggers" in cmd:
+            if cmd[-1] == "1":
+                self.log.info(f"Showing triggers")
+            elif cmd[-1] == "0":
+                self.log.info(f"Hiding triggers")
             for i in self.objectRegistry:
                 from src.GameObjects.TriggerBox import TriggerBox
                 if issubclass(type(i), TriggerBox):
@@ -121,9 +161,12 @@ class Application(ShowBase):
                         i.node.hide()
         elif "map" in cmd:
             mapName = cmd.split("map ")[1]
+            self.log.info(f"Loading map: {mapName}")
             self.mapLoader.loadMap(mapName)
+            self.log.debug(f"Successfully loaded map")
         elif "show collision" in cmd:
             if cmd[-1] == "1":
+                self.log.info(f"Showing collisions")
                 debugNode = BulletDebugNode('Debug')
                 debugNode.showWireframe(True)
                 debugNode.showConstraints(True)
@@ -134,15 +177,39 @@ class Application(ShowBase):
 
                 self.world.setDebugNode(self.debugNP.node())
             elif cmd[-1] == "0":
+                self.log.info(f"Hiding collisions")
                 self.debugNP.removeNode()
                 self.world.clearDebugNode()
         elif "stop" in cmd:
+            self.log.info(f"Stopping physics")
             self.doPhysics = False
         elif "start" in cmd:
+            self.log.info(f"Starting physics")
             self.doPhysics = True
         else:
-            print("No Command found")
+            self.log.info(f"No Command found")
     
     def createBulletWorld(self):
         self.world = BulletWorld()
         self.world.setGravity(Vec3(0, 0, -9.81))
+    
+    # Generates logging object to log to python console and a file
+    def getLogger(self, name):
+        # Check if logs directory exists, if not create it
+        import os
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+        
+        if name == "root":
+            if os.path.exists(Path("logs/log.log")):
+                os.remove(Path("logs/log.log"))
+        
+        log = logging.getLogger(name)
+        log.addHandler(logging.StreamHandler())
+        FileHandler = logging.FileHandler(Path("logs/log.log"))
+        logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(name)s] [%(levelname)-5.5s] %(message)s")
+        FileHandler.setFormatter(logFormatter)
+        log.addHandler(FileHandler)
+        
+        return log
+        
