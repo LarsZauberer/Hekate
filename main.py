39ef220d79bfd549aea27aee65295d3c2e52521b
LarsZauberer <wasser.ian@gmail.com>
2021-07-24 13:45:30 +0200

    init in main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from src.Application import Application
 
 class App(Application):
+    def __init__(self):
+        super().__init__()
+        # keybinds
+        
+        # Mousecatching
+        # self.catchMouse(True)
+        
+        # Other important stuff for your game
+    
     def update(self, task):
         super().update(task)
         return task.cont
